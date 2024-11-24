b47e90e1738da08e7f96f31231021e2e58fa3d3f
abhishekkjainn <jainabhishek1904@gmail.com>
2024-11-25 02:17:42 +0530

    Added page1 with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from './components/header';
+import Page1 from './pages/page1';
 
 function App() {
   return (
     <>
       <Header />
+      <Page1 />
     </>
   );
 }
---
+++ src/pages/page1.jsx
@@ -0,0 +1,21 @@
+export default function Page1() {
+  return (
+    <>
+      <div className="page1">
+        <div className="topname">Shailee Jain</div>
+        <div className="slogan">
+          From First Steps to Final Exams <br /> 15 Years of All-Subject
+          Excellence
+        </div>
+        <div className="cta">
+          <div className="primary-button">
+            See Results <img className="icon" src="arrow.png" />
+          </div>
+          <div className="secondary-button">
+            Achievements <img className="icon-secondary" src="arrow.png" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
