8ae95d1e92db11d4fce6d4f42c119aebb4c003eb
abhishekkjainn <jainabhishek1904@gmail.com>
2024-11-25 02:25:36 +0530

    Fixed logo sizing issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 
 import Header from './components/header';
 import Page1 from './pages/page1';
+import Page2 from './pages/page2';
 
 function App() {
   return (
     <>
       <Header />
       <Page1 />
+      <Page2 />
     </>
   );
 }
